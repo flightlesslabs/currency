989707b15d63599ce02ff0fd8e29b4d1ed4ec42c
Siddhesh Mangela <sidthecool007@gmail.com>
2026-04-13 19:40:32 +0530

    jsr publish added

---
+++ scripts/build_npm.ts
@@ -0,0 +1,31 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from '@deno/dnt';
+const pkg = JSON.parse(await Deno.readTextFile('./deno.json'));
+
+await emptyDir('./dist');
+
+await build({
+  entryPoints: ['./src/mod.ts'],
+  outDir: './dist',
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: pkg.name,
+    version: pkg.version,
+    publishConfig: {
+      'access': 'public',
+    },
+    description: 'Your package.',
+    license: 'MIT',
+    repository: {
+      type: 'git',
+      url: 'git+https://github.com/flightlesslabs/currency.git',
+    },
+  },
+  postBuild() {
+    Deno.copyFileSync('README.md', 'dist/README.md');
+  },
+});
