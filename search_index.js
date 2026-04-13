f274dfdae4af98bee674009cf229065d05b8bfa1
siddacool <siddacool@users.noreply.github.com>
2026-04-13 13:13:03 +0000

    deploy: 7ea41e81b923f8a9ba1febd4ab55730433b44ed9

---
+++ search_index.js
@@ -0,0 +1,3 @@
+(function () {
+  window.DENO_DOC_SEARCH_INDEX = {"kind":"search","nodes":[{"id":"namespace_add","kind":[{"char":"f","kind":"Function","title":"Function"}],"name":"add","file":".","doc":"","url":"././~/add.html","deprecated":false}]};
+})()
